673ad6aa4a91cd31944cd11101439a0de981ebe5
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2022-10-26 17:28:53 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__main__)
+@app.route('/')
+def return_root_message():
+  return "I'm alive. Surprising right? Anyway ill start duino.py now."
+
+if __name__ == '__main__':
+  app.run()
+  os.system("duino.py")
